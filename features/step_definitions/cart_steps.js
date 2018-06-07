09df49c3345e49a98eff6ea3030c98ceb354186b
Oriol <oriol@MacBook-Air-de-Sergio.local>
2018-06-07 12:20:59 +0200

    Pull realizado para empezar a cerrar todas las ramas hasta volver a la metodología de trabajar con copias de master, feature pendiente aun de cambios

--- features/step_definitions/cart_steps.js
+++ features/step_definitions/cart_steps.js
@@ -7,61 +7,85 @@ let alert = "";
 defineSupportCode(function ({Given, When, Then}) {
 
   Given(/^I've selected a few potates$/, function(){
+
     selectingPotatoes(3);
+
   });
 
   When(/^I check the cart$/, function(){
+
     browser.click("button#dropdownCart");
     totalPrice = browser.getText("td#totalPrice");
+
   });
 
   Then(/^I must see the cost of my purchase$/, function(){
+
     assert.notEqual(totalPrice, "0.00€");
+
   });
 
   Given(/^I've selected two potatoes$/, function(){
+
     selectingPotatoes(2);
+
   });
 
   When(/^I check the number of potatoes on the cart$/, function(){
+
     browser.click("button#dropdownCart");
     quantity = browser.getText("td.itemQuantity");
+
   });
 
   Then(/^I must see two potatoes too$/, function(){
+
     assert.equal(quantity, "2x");
+
   });
 
   Given(/^I've alredy selected some potatoes$/, function(){
+
     selectingPotatoes(5);
+
   });
 
   When(/^I press the clear button on the cart$/, function(){
+
     browser.click("button#dropdownCart");
     totalPrice = browser.getText("td#totalPrice");
     browser.click("button#clearcart");
     totalPrice = browser.getText("td#totalPrice");
+
   });
 
   Then(/^the cart must be empty$/, function(){
+
     assert.equal(totalPrice, "0.00€");
-  })
+
+  });
 
   Given(/^I've clear the cart with anything that have it$/, function(){
+
     selectingPotatoes(5);
+
   });
 
   When(/^I press the buy button$/, function(){
+
     browser.click("button#dropdownCart");
     browser.click("button#clearcart");
     totalPrice = browser.getText("td#totalPrice");
     assert.equal(totalPrice, "0.00€");
     browser.click("button#purchase");
+
   });
 
   Then(/^I must see the alert "You need to first add items to your cart"$/, function(){
+
     alert = browser.getText("div#alert");
     assert.equal(alert, "You need to first add items to your cart\n×");
+    
   })
 })
 
