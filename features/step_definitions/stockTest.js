1ad7fbb6a153c28b1396ade9af9492fb8090e3f2
alejandro <squella100@gmail.com>
2018-06-11 11:56:44 +0200

    stock Terminado

--- features/step_definitions/stockTest.js
+++ features/step_definitions/stockTest.js
@@ -1,31 +1,18 @@
 const {Given, When, Then} = require('cucumber');
-const assert = require ('assert').strict;
+const {getTexto,comprarProductos,validarStock,limpiarCarrito} =
+require('../components_functions/stock_function');
+
 const urlPotatoShop = "http://localhost:4200/";
+const timeOut = 100000;
 var numeroDeCompras;
 var stockRusserPotato_inicial;
 var comprarRusserPotato;
 var stockRusserPotato_final;
 var validar;
 
-
-
-function getTexto(selector,index){
-    return ($$(selector).map((elemento) => elemento.getText()))[index];
-}
-function comprarProductos(selectorProducto, cantidad, index){
-    for(let i = 0; i<cantidad; i++){
-        $$(selectorProducto)[index].click();
-    }return cantidad;
-}
-function validarStock(stockInicial, stockFinal, numeroDeCompras){
-    let res = stockInicial - numeroDeCompras;
-    return parseInt(stockFinal) === res;
-}
-
 Given(/^I am on potatoeshop page$/,()=>{
     browser.url(urlPotatoShop);
 });
-
 When(/^I add a product to cart$/,()=>{ 
     numeroDeCompras=1;
     stockRusserPotato_inicial = getTexto('.col.stock',3);
@@ -33,8 +20,7 @@ When(/^I add a product to cart$/,()=>{
 });
 Then(/^discount the product from stock$/,()=>{
     stockRusserPotato_final = getTexto('.col.stock',3);
-    validar = validarStock(stockRusserPotato_inicial,stockRusserPotato_final, comprarRusserPotato);
-    browser.pause(2000);       
+    validar = validarStock(stockRusserPotato_inicial,stockRusserPotato_final, comprarRusserPotato); 
 });
 
 Given(/^I add all product to cart$/,()=>{
@@ -42,41 +28,47 @@ Given(/^I add all product to cart$/,()=>{
         numeroDeCompras += 7;//8 es el stockInicial que hay de este producto (7+1=8)
         stockRusserPotato_inicial = getTexto('.col.stock',3);
         comprarRusserPotato = comprarProductos('.add-to-cart',numeroDeCompras,3);
-    }else{browser.pause(100000);}//lo he puesto para que falle el test, si conocen otra forma de que pare el test si el stock es incorrecto (=false) lo comentan :) 
+    }else{browser.pause(timeOut);}
 });
 When(/^I dont have stock of that product$/,()=>{
-    browser.pause(2000);
     browser.click('#alert');
-    browser.pause(2000);
-       
-    console.log('/n recodemos que tiene que ser 8, numeroDeCompras= '+numeroDeCompras);
-    //browser.pause(8000);
 }); 
 Then(/^I have all product in my cart$/,()=>{
-
+    browser.click('#dropdownCart');
+    let comprobarStock = getTexto('.itemQuantity',0);
+    if(comprobarStock === '8x'){
+        browser.click('#clearcart');//limpio el carrito
+    }else{browser.pause(timeOut);}
 });
-
-
+//
 Given(/^there arent stock of product$/,()=>{
-
+    numeroDeCompras = 25;
+    stockRusserPotato_inicial = getTexto('.col.stock',0);
+    comprarRusserPotato = comprarProductos('.add-to-cart',numeroDeCompras,0);
 });
 When(/^I add the product in my cart$/,()=>{
-
+    browser.click('.add-to-cart');
 });
 Then(/^I cant add the product$/,()=>{
-
+    browser.click('#alert'); 
 });
   
-
 Given(/^I add all product in my cart$/,()=>{
-
+    limpiarCarrito();
+    numeroDeCompras = 25;
+    comprarRusserPotato = comprarProductos('.add-to-cart',numeroDeCompras,0);
+    stockRusserPotato_inicial = getTexto('.col.stock',0);//0
 });
 When(/^I was wrong to buy the product$/,()=>{
-
+    browser.click('.add-to-cart');
 });
 Then(/^I clear the cart$/,()=>{
-
+    browser.click('#alert');
+    limpiarCarrito();
 });
 Then(/^the stock of product come back the initial state$/,()=>{
-
-});  
+    stockRusserPotato_inicial = getTexto('.col.stock',0);//25
+    if(stockRusserPotato_inicial === '25'){
+        browser.click('.close');
+    }else{browser.pause(timeOut);}
+});
