d33fd8231c9f84db9eb76161f70faa850ab943b7
alejandro <squella100@gmail.com>
2018-06-12 14:02:06 +0200

    componente buy terminado

--- features/step_definitions/buyTest.js
+++ features/step_definitions/buyTest.js
@@ -27,36 +27,26 @@ var validar;
   });
  Then('buy the potatoes',()=>{
   if(validar === true){
-     let comprar = buyProducts();
+    assert.equal(buyProducts(),true);
   }else{browser.pause(timeOut);}
-
  });
 
  Given('I dont have a enough money',()=>{
-
+    limpiarCarrito();
  }); 
  When('I try to pay the potatoes',()=>{
-
- }); 
+   browser.click('#dropdownCart');
+   browser.click('#purchase');
+}); 
  Then('I cant buy it',()=>{
-
+    browser.waitForVisible('#alert')
+    browser.click('#alert');
  });
 
 
 /* Sample_Syntax */
 /*
-defineSupportCode(function ({Given, When, Then}) {
-
-  Given(/^I want to buy some potatoes$/, function(){
-    browser.click("button.btn.btn-primary");
-  });
-
-  When(/^I click to buy on the cart after selecting some potatoes$/, function(){
-    browser.click("button#dropdownCart");
-    browser.click("button#purchase");
-  });
-
-  Then(/^I must see "Thank you for your purchase!" on my screen$/, function(){
+Then(/^I must see "Thank you for your purchase!" on my screen$/, function(){
     const checkOk = "div.modal-header h4.modal-title";
     checkOk.includes("Thank you for your purchase!");
   })
