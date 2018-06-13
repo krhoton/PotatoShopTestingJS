99d606fa60211efe4d46b04e074ec08fe825e421
oMunozB <oriolmbravo@gmail.com>
2018-06-13 17:44:04 +0200

    Todo perfectamente implementado exceptuando la comprobación de la existencia del archivo

--- features/step_definitions/download_steps.js
+++ features/step_definitions/download_steps.js
@@ -1,5 +1,8 @@
 const assert = require('assert').strict;
+const fs = require('fs');
 const {Given, When, Then} = require('cucumber');
+
+/*--- Mirar link: https://nodejs.org/api/fs.html#fs_file_system (acces y stat)---*/
 
 Given (/^I've made an order$/, function(){
 
@@ -9,13 +12,18 @@ Given (/^I've made an order$/, function(){
 
 When (/^I downloaded it$/, function(){
 
-
+  browser.click("button#dropdownCart");
+  browser.click("button#purchase");
+  browser.click("div.modal-body a");
 
 });
 
 Then (/^It must appear in my computer$/, function(){
 
-  
+  let file_route = "./downloaded_orders/order.txt"
+  fs.access(file_route, fs.constants.F_OK, (err) => {
+    console.log(`${file_route} ${err ? 'does not exist' : 'exists'}`);
+  });
 
 });
 
