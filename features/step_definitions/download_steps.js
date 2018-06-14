209eb7222a2ced3c7ae965312d2d9bcfbd9f1e14
oMunozB <oriolmbravo@gmail.com>
2018-06-14 10:53:49 +0200

    Estructura realizada del feature Download y casi acabada

--- features/step_definitions/download_steps.js
+++ features/step_definitions/download_steps.js
@@ -1,6 +1,7 @@
 const assert = require('assert').strict;
 const fs = require('fs');
 const {Given, When, Then} = require('cucumber');
+let exists = false;
 
 /*--- Mirar link: https://nodejs.org/api/fs.html#fs_file_system (acces y stat)---*/
 
@@ -15,16 +16,21 @@ When (/^I downloaded it$/, function(){
   browser.click("button#dropdownCart");
   browser.click("button#purchase");
   browser.click("div.modal-body a");
+  browser.pause(2500);
 
 });
 
 Then (/^It must appear in my computer$/, function(){
 
   let file_route = "./downloaded_orders/order.txt"
-  fs.access(file_route, fs.constants.F_OK, (err) => {
-    console.log(`${file_route} ${err ? 'does not exist' : 'exists'}`);
-  });
 
+  fs.access(file_route, fs.constants.F_OK, function(err) {
+    if (err) {
+    } else {
+      exists = true;
+    }
+    assert.equal(exists, true);
+  });
 });
 
 
