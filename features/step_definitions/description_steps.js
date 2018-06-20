d3d9c412da30c5e1967ad51e1234128d2d199371
alejandro <squella100@gmail.com>
2018-06-20 09:42:09 +0200

    steps_description.js añadido

---
+++ features/step_definitions/description_steps.js
@@ -0,0 +1,14 @@
+const {Given, When, Then} = require('cucumber');
+const assert = require ('assert').strict;
+
+Given('I want to information about of some product',()=>{
+
+});
+
+When('I select a product for more information',()=>{
+    
+});
+
+Then('I read the product description',()=>{
+    
+});
