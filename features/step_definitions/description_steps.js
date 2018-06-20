6ca99efebcd89e194f3e993cfbb3544546057d85
alejandro <squella100@gmail.com>
2018-06-20 10:04:30 +0200

    description realizada

--- features/step_definitions/description_steps.js
+++ features/step_definitions/description_steps.js
@@ -1,14 +1,12 @@
 const {Given, When, Then} = require('cucumber');
 const assert = require ('assert').strict;
 
-Given('I want to information about of some product',()=>{
-
-});
+Given('I want to information about of some product',()=>{});
 
 When('I select a product for more information',()=>{
-    
+    browser.click('.col-8.item');
 });
 
 Then('I read the product description',()=>{
-    
+    assert.equal(browser.getText('.col.item-details'),'Because of their waxy texture, red potatoes stay firm throughout cooking, whether roasted or stewed. Their thin yet vivid red skin adds appealing color and texture to sides and salads. Reds are frequently used to make tender yet firm potato salad and to add pizazz to soups and stews. They are also delicious mashed or roasted.');
 });
